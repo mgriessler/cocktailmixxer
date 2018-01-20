a0dabf31da0f2384d8645aa2735f7bc6f8128731
mgriessler <mgriessler@ufl.edu>
2018-01-20 22:01:09 +0000

    getting ready to merge

--- js/drink_manager.js
+++ js/drink_manager.js
@@ -1,19 +1,42 @@
 var buttons = new Array();
 function consoleDrink() {
-    console.log("here");
-    console.log(drinks);
 }
 
 function genDrinkList() {
     $.each(drinks.drinks, function(i,drink) {
-        console.log(drink.name);
         var b = $('<button class="button"/>')
             .text(drink.name)
-            .click(function() {alert(drink.glass);});
+            .click(function() {
+                populateDrink(drink); 
+            });
         buttons.push(b);
         $(".left-side-bar").append(b);
     });
-    /*$.each(buttons, function(i,b) {
-        $(".left-side-bar").append(b);
-    });*/
+}
+
+function populateDrink(drink) {
+    populateIngredients(drink.ingredients);
+    populatePrepInstructions(drink.prep);
+    populateLinks(drink.links);
+}
+
+function populateIngredients(ingredients) {
+    var ingredientNames = new Array();
+    var ingredientParts = new Array();
+    var ingredientText = new Array();
+    $.each(ingredients, function(key, ingredient) {
+        if(ingredient.cl) {
+            ingredientNames.push(ingredient.name);
+            ingredientParts.push(ingredient.part);
+            ingredientText.push(ingredient.name + " - " + ingredient.part + " part(s)");
+        }
+    }); 
+}
+function populatePrepInstructions(prep) {
+    $("#prep-instructions").text(prep);
+}
+
+function populateLinks(links) {
+    var linkhtml = "<ul><li><a href='" + links[0] + "'>Wikipedia</a></li><li><a href='" + links[1] + "'>International Bartenders Association</a></li></ul>";
+    $("#links").html(linkhtml);
 }
--- js/drinks.js
+++ js/drinks.js
@@ -37,5 +37,70 @@ var drinks = {"drinks": [
         ],
         "served": "on the rocks",
         "timing": "any"
+    },
+    {
+        "name": "Mojito",
+        "ingredients": [
+            { "name": "white rum", "part": 4, "cl": 4 },
+            { "name": "lime juice", "part": 3, "cl": 3 },
+            { "name": "mint", "part": 6, "unit": "spring" },
+            { "name": "sugar", "part": 2, "unit": "tsp" },
+            { "name": "soda water", "part": 3, "cl": 3 }
+        ],
+        "prep": "Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with the remaining soda water.",
+        "links": [
+            "https://en.wikipedia.org/wiki/Mojito",
+            "http://www.iba-world.com/english/cocktails/mojito"
+        ],
+        "glass": "collins glass",
+        "garnishes": [
+            "sprig of mint leaves",
+            "lemon slice",
+            "straw"
+        ],
+        "served": "on the rocks",
+        "timing": "any"
+    },
+    {
+        "name": "Long Island Iced Tea",
+        "ingredients": [
+            { "name": "tequila", "part": 3, "cl": 1.5 },
+            { "name": "vodka", "part": 3, "cl": 1.5 },
+            { "name": "white rum", "part": 3, "cl": 1.5 },
+            { "name": "triple sec", "part": 3, "cl": 1.5 },
+            { "name": "gin", "part": 3, "cl": 1.5 },
+            { "name": "lemon juice", "part": 5, "cl": 2.5 },
+            { "name": "simple syrup", "part": 6, "cl": 3 },
+            { "name": "coca-cola", "part": 1, "unit": "splash" }
+        ],
+        "prep": "Add all ingredients into high ball glass filled with ice. Stir gently.",
+        "links": [
+            "https://en.wikipedia.org/wiki/Long_Island_Iced_Tea",
+            "http://www.iba-world.com/english/cocktails/long-island-iced-tea"
+        ],
+        "glass": "highball glass",
+        "garnishes": [
+            "lemon spiral",
+            "straw"
+        ],
+        "served": "on the rocks",
+        "timing": "any"
+    },
+    {
+        "name": "Grasshopper",
+        "ingredients": [
+            { "name": "creme de menthe", "part": 1, "cl": 3 },
+            { "name": "creme de cacao", "part": 1, "cl": 3 },
+            { "name": "fresh cream", "part": 1, "cl": 3 }
+        ],
+        "prep": "Pour into a cocktail shaker with ice. Shake briskly and then strain into a chilled cocktail glass.",
+        "links": [
+            "https://en.wikipedia.org/wiki/Grasshopper_(cocktail)",
+            "http://www.iba-world.com/english/cocktails/grasshopper"
+        ],
+        "glass": "cocktail glass",
+        "garnishes": [],
+        "served": "straight up",
+        "timing": "after dinner"
     }
 ]};
