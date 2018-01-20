41bd8bcc44a9e38b2818edff16d84932b2a269fa
root <root@ip-10-0-0-155.ec2.internal>
2018-01-20 14:32:10 +0000

    initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//this function is called when the DOM is ready apparently is equivalent to $(document).reday(function(){});
+$(function() {
+    var c = $("#drink")[0];
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = "green";
+    ctx.fillRect(10,10,100,100);
+    ctx.moveTo(0,0);
+    ctx.lineTo(200,100);
+    ctx.stroke();
+});
